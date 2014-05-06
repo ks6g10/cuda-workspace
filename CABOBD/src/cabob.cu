19808cf6abbf1cc537a1b3eef38ad73feeacec4a
ks6g10 <ks@linux.com>
2014-05-06 22:28:22 +0100

    TAG: The recursion

--- CABOBD/src/cabob.cu
+++ CABOBD/src/cabob.cu
@@ -24,21 +24,6 @@
 					exit(1);					\
 		} }
 
-__device__ unsigned int bitreverse(unsigned int number) {
-	number = ((0xf0f0f0f0 & number) >> 4) | ((0x0f0f0f0f & number) << 4);
-	number = ((0xcccccccc & number) >> 2) | ((0x33333333 & number) << 2);
-	number = ((0xaaaaaaaa & number) >> 1) | ((0x55555555 & number) << 1);
-	return number;
-}
-template<int _rows,int _collumns>
-
-/**
- * CUDA kernel function that reverses the order of bits in each element of the array.
- */
-__global__ void bitreverse(void *data) {
-	unsigned int *idata = (unsigned int*) data;
-	idata[threadIdx.x] = bitreverse(idata[threadIdx.x]);
-}
 
 struct stats {
 	int collumns;
@@ -155,11 +140,13 @@ __device__ void pivot(float  (*p)[rows],float * cost, int collumns, int collumn,
  */
 //only works for 32 rows!!
 template<int rows>
-__device__ float __forceinline__ apply_row_op(float  (*p)[rows],float * cost, int collumns)
+__device__ float __forceinline__ apply_row_op(float  (*p)[rows],float * cost, int collumns,unsigned short * basis)
 {
 	//__shared__ int bids[32][32];
 	//bids[warpid][laneid] = -1;
 	//assert(cost[0] == -2.0f);
+	//(collumns-rows-2) = n bids
+	basis[threadIdx.x%32] = (collumns-rows-2) + (threadIdx.x%32);
 	while(1) {
 		int row, collumn;
 		collumn = bland(cost,collumns);
@@ -167,14 +154,17 @@ __device__ float __forceinline__ apply_row_op(float  (*p)[rows],float * cost, in
 			break;
 		}
 		row = get_pivot_row<rows>(p,collumn,collumns);
+		if(threadIdx.x%32 == 0) {
+			basis[collumn] = row;
+		}
 		//printf("lane %d col %d row %d cost %f\n",laneid,collumn,row,cost[collumn]);
-	//	if(row == -1) {
+		//	if(row == -1) {
 		//	count = 2;
 		//}
 		//		if(laneid == 0) {
 		//			printf("warpid %d col %d row %d %f\n",warpid,collumn,row,p[collumn][row]);
 		//		}
-	//	assert(row >= 0);
+		//	assert(row >= 0);
 		pivot<rows>(p,cost,collumns,collumn,row);
 		__threadfence();
 		//bids[warpid][]
@@ -185,14 +175,41 @@ __device__ float __forceinline__ apply_row_op(float  (*p)[rows],float * cost, in
 	return cost[collumns-1];
 }
 
+template<int size>
+struct stack {
+	unsigned int variables[32];
+	unsigned int confs[size];
+	unsigned int value[size];
+};
+
+enum estack{
+	VALUE = 0,
+	CONF = 1,
+	SIZE = 2,
+	OFFSET = 3
+};
+
+enum rstatus {
+	COMPLETE = -1,
+	NOEDGE = -2
+};
+
+
 template<int rows>
 __device__ int __forceinline__  init_table(float  (*matrix)[rows],float * cost,int n, unsigned int * in, unsigned int * in_value) {
 	const unsigned int tid = threadIdx.x;
 	const unsigned int laneid = tid % 32;
 	const unsigned int mask =  1 << laneid;
+	int count = 0;
 	int i;
 	for(i = 0; i < n; i ++) {
+		count += ((in[i]  & mask) > 0);
 		matrix[i][laneid] =(float) ((in[i]  & mask) > 0);
+	}
+	if(__ballot(count == n)) {
+		return COMPLETE;//COMPLETE
+	} else if (!__ballot(count > 1)) {
+		return NOEDGE;//NO EDGES
 	}
 	//now i == n
 	for(;i<n+32;i++) {
@@ -220,45 +237,204 @@ __device__ int __forceinline__  init_table(float  (*matrix)[rows],float * cost,i
 }
 
 
-template<int rows>
-__global__ void do_simplex(float  (*matrix)[rows],float * cost,int n,unsigned int * in, unsigned int * in_value) {
+template<int rows, int stack_size,int stacks_per_warp>
+__device__ int add_initial_bids(struct stack<stack_size> * mystack, int index, int bids, unsigned int * in, unsigned int * in_value, unsigned int laneid) {
+	int size = 0;
+	const unsigned int root = in[index];
+	const unsigned int ltmask = (1 << laneid) - 1;
+	for(int i = laneid+index+1; i < bids; i += 32) {
+		unsigned int conf = in[i];
+		unsigned int status = (conf & root);
+		unsigned int status2;
+		if((status2 = __ballot(status == 0))) {
+			int put = __popc((status2 & ltmask));
+			if(status == 0) {
+				mystack->confs[put+size] = conf;
+				mystack->value[put+size] = in_value[i];
+			}
+			size += __shfl(put+(status == 0),31);
+		}
+	}
+	if(laneid == 0) {
+		mystack->variables[VALUE] = in_value[index];
+		mystack->variables[CONF] = root;
+		mystack->variables[SIZE] = size;
+		mystack->variables[OFFSET] = 0;
+	}
+	return size;
+
+}
+
+template<int rows, int stack_size,int stacks_per_warp>
+__device__ int __forceinline__ add_bids_new_stack(struct stack<stack_size> * parent,struct stack<stack_size> * child,int index,unsigned int laneid, unsigned int backtrack) {
+
+	int size = 0;
+
+	if(parent->variables[SIZE] == 0) {
+		return 0;
+	}
+	if(child->variables[OFFSET] == 0 && backtrack) {
+		return -1;
+	}
 
 
-	const unsigned int warpid = (threadIdx.x+ blockIdx.x * blockDim.x) / 32;
+	const unsigned int root = parent->confs[index] | parent->variables[CONF];
+	const unsigned int ltmask = (1 << laneid) - 1;
+	if(laneid == 0) {
+		parent->variables[SIZE] -=1;
 
-	//__shared__ int cache2[1024];
-	const int new_n = n + 32 + 2;
-	init_table<rows>((matrix+warpid*new_n),(cost+warpid*new_n),n,in,in_value);
-	//	__syncthreads();
+		child->variables[VALUE] = parent->value[index] + parent->variables[VALUE];
+	}
 
-	//	float  (*p)[rows] = (matrix+warpid*new_n);
-	//	float  (*p2)[rows]= (matrix+(warpid+1)*new_n);
-	//	if(warpid < 31) {
-	//		for(int i = 0; i < new_n;i++) {
-	//			assert(p[i][laneid] == p2[i][laneid]);
-	//		}
-	//	}
-	//
-	//	return;
-	apply_row_op<rows>((matrix+warpid*new_n),(cost+warpid*new_n),new_n);
-	//	__threadfence_system();
-	//__syncthreads();
-	//assert(cache2[tid] == cache2[(tid+32)%blockDim.x]);
-	//assert(collumn == cache2[tid]);
-	//	printf("t1 %d i %d, warp %d\n",cache2[tid],tid,warpid);
+	//remove the added bid from prev graph
+	for(int i = index+laneid+1; i < parent->variables[SIZE]+1; i +=32) {
+		unsigned int conf = parent->confs[i];
+		unsigned int value = parent->value[i];
+		parent->confs[i-1] = conf;
+		parent->value[i-1] = value;
+	}
 
-	//assert(cache2[i] == cache2[tid]);
-	//assert(cache[laneid][laneid] == cache[warpid][laneid]);
+
+
+	for(int i = laneid; i < parent->variables[SIZE]; i += 32) {
+		unsigned int conf = parent->confs[i];
+		unsigned int status = (conf & root);
+		unsigned int status2;
+		if((status2 = __ballot(status == 0))) {
+			int put = __popc((status2 & ltmask));
+			if(status == 0) {
+				child->confs[put+size] = conf;
+				child->value[put+size] = parent->value[i];
+			}
+			size += __shfl(put+(status == 0),31);
+		}
+	}
+
+
+
+	if(laneid == 0) {
+
+		child->variables[CONF] = root;
+		child->variables[SIZE] = size;
+		child->variables[OFFSET] = parent->variables[OFFSET]+1;
+	}
+
+
+	return size;
+}
+
+template<int rows, int stack_size,int stacks_per_warp>
+__device__ void __forceinline__ recursion(struct stack<stack_size> * stack,unsigned int * global_max,float  (*matrix)[rows],float * cost, unsigned short * basis,const int re) {
+	const unsigned int laneid = threadIdx.x%32;
+	if(re >= rows) {
+		return;
+	}
+	if(stack->variables[SIZE] == 0) {
+		return;
+	}
+	int status = init_table<rows>(matrix,cost,stack->variables[SIZE],stack->confs,stack->value);
+
+	if(status == COMPLETE) {
+		unsigned int tmax = 0;
+		for(int i = laneid;i < stack->variables[SIZE];i +=32) {
+			tmax = max(tmax,stack->value[i]);
+		}
+
+		tmax = max(tmax,(unsigned int)__shfl_xor((int)tmax,16));
+		tmax = max(tmax,(unsigned int)__shfl_xor((int)tmax, 8));
+		tmax = max(tmax,(unsigned int)__shfl_xor((int)tmax, 4));
+		tmax = max(tmax,(unsigned int)__shfl_xor((int)tmax, 2));
+		tmax = max(tmax,(unsigned int)__shfl_xor((int)tmax, 1));
+
+		if(laneid == 0) {
+			atomicMax(global_max,tmax+stack->variables[VALUE]);
+		}
+		return;
+	} else if(status == NOEDGE) {
+		unsigned int tmax = 0;
+		for(int i = laneid;i < stack->variables[SIZE];i +=32) {
+			tmax += stack->value[i];
+		}
+
+		tmax += (unsigned int)__shfl_xor((int)tmax,16);
+		tmax += (unsigned int)__shfl_xor((int)tmax,8);
+		tmax += (unsigned int)__shfl_xor((int)tmax,4);
+		tmax += (unsigned int)__shfl_xor((int)tmax,2);
+		tmax += (unsigned int)__shfl_xor((int)tmax,1);
+
+		if(laneid == 0) {
+			atomicMax(global_max,tmax+stack->variables[VALUE]);
+		}
+		return;
+	}
+
+	unsigned int estimate = apply_row_op<rows>(matrix,cost,stack->variables[SIZE]+32 + 2,basis);
+	estimate += stack->variables[VALUE];
+	if(estimate < *global_max) {
+		return;
+	} else if(is_integer<rows>(matrix,stack->variables[SIZE] + 32 + 2)) {
+		atomicMax(global_max,estimate);
+		return;
+	}
+	while(stack->variables[SIZE]) {
+		int size= add_bids_new_stack<rows,stack_size,stacks_per_warp>(stack,(stack+1),0,laneid,0);
+		if(size == 0) continue;
+
+		recursion<rows,stack_size,stacks_per_warp>((stack+1),global_max,matrix,cost,basis,re+1);
+	}
+	return;
+}
+
+#define MOFFSET (gwarpid*(n + 32 + 2))
+template<int rows, int stack_size,int stacks_per_warp>
+__global__ void do_simplex(struct stack<stack_size> * stacks,unsigned int * global_max, unsigned int * atom, float  (*matrix)[rows],float * cost,int n,unsigned int * in, unsigned int * in_value) {
+
+
+	const unsigned int gwarpid = (threadIdx.x+ blockIdx.x * blockDim.x) / 32;
+	const unsigned int warpid = threadIdx.x/32;
+	const unsigned int laneid = threadIdx.x%32;
+	__shared__ struct stack<stack_size> * stack[rows];
+	__shared__ unsigned short basis[32][rows];
+	if(laneid == 0) {
+		stack[warpid] = (stacks+gwarpid*stacks_per_warp);
+	}
+	newalloc:
+	int index = -1;
+	if(laneid == 0) {
+		index = atomicAdd(atom,1);
+	}
+	index = __shfl(index,0);
+	if(index >= n) {
+		//if(laneid == 0)
+		//printf("%u\n",global_max);
+		return;
+	}
+	if(add_initial_bids<rows,stack_size,stacks_per_warp>(stack[warpid],index,n,in,in_value,laneid) == 0) {
+		goto newalloc;
+	}
+
+	recursion<rows,stack_size,stacks_per_warp>(stack[warpid],global_max,(matrix+MOFFSET),(cost+MOFFSET),basis[warpid],0);
+
+	goto newalloc;
 
 }
 
 #define set(X) (1<<(X))
 #define set2(X,Y) (set(X)|set(Y))
 int main(int argc, const char* argv[]) {
-	int warps = (1024*14)/32;
+	//int warps = (1024*14)/32;
 	const int problemwidth = 21;
 	const int rows = 32;
 	const int collumns = problemwidth+2+rows;
+	const int blocks = 7;
+	const int threads = 1024;
+	const int warps = blocks*(threads/32);
+
+	const int stack_width = rows;
+	const int stacks_per_warp = rows;
+	const int stacks = warps*stacks_per_warp;
+
+
 	float matrix[collumns][rows];
 	unsigned int bids[problemwidth] = {set(5),set2(4,5),set2(2,4),set2(2,3),set(3),set2(1,3),
 			46554,88465,122321,4654848,6545645,1321321,3215484,64555,
@@ -314,13 +490,23 @@ int main(int argc, const char* argv[]) {
 	printf("allocated %lu bytes for matrix\n",sizeof(matrix)*warps);
 	float * mmatrix = (float *)  dmatrix;
 	float * dcost;
+	unsigned int * dmax;//
+	unsigned int * datom;
 	unsigned int * in_value;
 	unsigned int * in;
+	struct stack<stack_width> * dstacks;
+
 	CUDA_CHECK_RETURN(cudaDeviceSetCacheConfig( cudaFuncCachePreferL1 ));
+	CUDA_CHECK_RETURN(cudaMalloc((void**) &dmax,sizeof(unsigned int)));
+	CUDA_CHECK_RETURN(cudaMalloc((void**) &datom,sizeof(unsigned int)));
+	CUDA_CHECK_RETURN(cudaMalloc((void**) &dstacks,sizeof(struct stack<stack_width>)*stacks));
+
 	CUDA_CHECK_RETURN(cudaMalloc((void**) &in,sizeof(unsigned int)*problemwidth));
 	CUDA_CHECK_RETURN(cudaMalloc((void**) &in_value,sizeof(unsigned int)*problemwidth));
 	CUDA_CHECK_RETURN(cudaMalloc((void**) &dcost,sizeof(cost)*warps));
 	printf("allocated %lu bytes for cost\n",sizeof(cost)*warps);
+	CUDA_CHECK_RETURN(cudaMemset(dmax,0,sizeof(unsigned int)));
+	CUDA_CHECK_RETURN(cudaMemset(datom,0,sizeof(unsigned int)));
 
 	CUDA_CHECK_RETURN(cudaMemcpy(in,bids,sizeof(unsigned int)*problemwidth, cudaMemcpyHostToDevice));
 	CUDA_CHECK_RETURN(cudaMemcpy(in_value,value,sizeof(unsigned int)*problemwidth, cudaMemcpyHostToDevice));
@@ -332,7 +518,7 @@ int main(int argc, const char* argv[]) {
 
 	//CUDA_CHECK_RETURN(cudaMalloc((void**) &d, sizeof(float) * 26843545));
 	printf("hello\n");
-	do_simplex<32><<<7,1024>>>(dmatrix,dcost,problemwidth,in,in_value);
+	do_simplex<rows,stack_width,stacks_per_warp><<<7,1024>>>(dstacks,dmax,datom,dmatrix,dcost,problemwidth,in,in_value);
 	CUDA_CHECK_RETURN(cudaThreadSynchronize());
 	//return 0;
 	CUDA_CHECK_RETURN(cudaMemcpy(matrix,mmatrix,sizeof(matrix), cudaMemcpyDeviceToHost));
